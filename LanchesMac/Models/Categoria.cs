4d5f224753c095485a1e8556388bbb3d67fd3ed1
Kaique Luciano Prestes de Oliveira Godinho <kaiqueluciano07@gmail.com>
2022-10-01 23:57:52 -0300

    Kaique --> Ajustes Migrations e DB

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [StringLength(100, ErrorMessage = "O tamanho máximo é de 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name ="Nome")]
+        public string CategoriaNome { get; set; }
+        [StringLength(200, ErrorMessage = "O tamanho máximo é de 100 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Descrição da categoria")]
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
