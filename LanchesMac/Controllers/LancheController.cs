635cd90b41c354e30bb2a1c52e0373cf25f83ff9
Kaique Luciano Prestes de Oliveira Godinho <kaiqueluciano07@gmail.com>
2022-10-03 19:46:09 -0300

    Kaique -- Ajustes views

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,23 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+
+        private readonly ILancheRepository _lancherepository;
+
+        public LancheController(ILancheRepository lancherepository)
+        {
+            _lancherepository = lancherepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancherepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
